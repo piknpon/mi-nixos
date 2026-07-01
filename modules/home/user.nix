e802d357402c7187994cea93a6ab27d83b80df43
davds <davdss@tuta.io>
2026-07-01 13:37:59 -0500

    Eliminar .gitkeep y asegurar default.nix válidos

---
+++ modules/home/user.nix
@@ -0,0 +1,9 @@
+{ ... }:
+{
+  # Esto es obligatorio para que Home Manager funcione
+  home.stateVersion = "24.05"; # Asegúrate de que coincida con tu versión de NixOS
+  
+  # Si tienes nombre de usuario distinto, también puedes ponerlo aquí:
+  # home.username = "ciruela";
+  # home.homeDirectory = "/home/ciruela";
+}
