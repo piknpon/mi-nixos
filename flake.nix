b4067742869c67a2562b3456ddde18641558a48f
davdss <davdss@tuta.io>
2026-07-01 11:47:23 -0500

    s

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs"
+  };
  
   outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations = { 
