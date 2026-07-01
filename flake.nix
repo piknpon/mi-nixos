b069d91fd72df3a8b06effc7e6b08a5b8c31a8fd
davdss <davdss@tuta.io>
2026-07-01 11:15:39 -0500

    home manager - flake

--- flake.nix
+++ flake.nix
@@ -3,17 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+    
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs"
+ 
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations = { 
+      
+      vm-red = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/vm-red/default.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ciruela = import ./modules/home/ciruela.nix;
+          }
+        ];
+      };
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.vm-red = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./hosts/vm-red/default.nix ];
-    };
-     
-    nixosConfigurations.vm-pink = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./hosts/vm-pink/default.nix ];
+      vm-pink = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/vm-pink/default.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ciruela = import ./modules/home/ciruela.nix;
+          };
+        ];
+      };
     };
   };
 }
